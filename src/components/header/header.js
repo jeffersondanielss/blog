dc5e6eca71386dc23540e97c3726ed8ac75caee4
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-24 15:04:20 -0300

    funcionalidade: header links

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import Github from 'react-icons/lib/go/mark-github';
+import Home from 'react-icons/lib/go/home';
+
 import './header.css';
 
 export default props =>
-  <div className="header">
-    <div className="wrapper">
-      <Link to="/" className="logo">
+  <div className='header'>
+    <div className='wrapper'>
+      <Link to='/' className='logo'>
         Jefferson Daniel
       </Link>
+
+      <nav className='header-nav'>
+        <a className='header-link' href='https://github.com/jeffersondanielss' target='_blank'>
+          <Github color='white' />  
+        </a>
+
+        <a className='header-link' href='http://jeffersondaniel.com'>
+          <Home color='white' />  
+        </a>
+      </nav>
     </div>
   </div>;
