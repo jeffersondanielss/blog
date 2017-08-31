c570cdec401ef97018c4763f669674777bd37520
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-31 19:13:01 -0300

    tarefa: página 404 temporária

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import error from '../images/error.gif'
+import Home from 'react-icons/lib/go/home';
+import Link from '../components/link';
+
+import '../css/not-found.css'
 
 export default () =>
-  <div className='wrapper'>
-    <h1>Página não encontrada</h1>
+  <div className='wrapper not-found'>
+    <h1 className='not-found-title'>Ops, página não encontrada...</h1>
     <img src={ error } /> 
+    <hr className='not-found-line'/>
+
+
+    <Link to="/" className='not-found-text'>
+      <Home /> Voltar para a Home
+    </Link>
+
   </div>;
