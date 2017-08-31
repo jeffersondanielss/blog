350b2aa1fecf7d86b96fca1afee568d3ae65c6b2
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-31 19:20:24 -0300

    correção: mudança da bio

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -13,15 +13,15 @@ export default ({ post, url }) => (
       <img className='avatar-image' src={ me } />
       <div className='avatar-info'>
         <h2 className='avatar-name'>Jefferson Daniel</h2>
-        <p className='avatar-description'>javascript writer</p>
+        <p className='avatar-description'>just a programmer</p>
       </div>
 
       <div className='social-media'>
         <span className='social-media-label'>Compartilhe: </span>
-        <a href={`https://www.facebook.com/sharer/sharer.php?u=${url}${post.path}`} target='_blank'>
+        <a href={`https://www.facebook.com/sharer/sharer.php?u=${url}${post.path}`} onclick="window.open(this.href, 'twitter-share', 'width=550,height=235');return false;">
           <Facebook color="#4267B2" />
         </a>
-        <a href={`https://twitter.com/intent/tweet?text=${post.title}&url=${url}${post.path}&hashtags=${post.tags}`} target='_blank'>
+        <a href={`https://twitter.com/intent/tweet?text=${post.title}&url=${url}${post.path}&hashtags=${post.tags}`} target='_blank' onclick="window.open(this.href, 'twitter-share', 'width=550,height=235');return false;">
           <Twitter color="#0FA7C2" />
         </a>
         <a href={`http://www.linkedin.com/shareArticle?mini=true&url=${url}${post.path}&title=${post.title}`} target='_blank'>
