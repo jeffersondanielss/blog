8a7d89d73195edc2b1688dbea9f0acaba322afc8
Jefferson Daniel <jeffersondanielss@gmail.com>
2020-05-29 23:51:53 -0300

    Updates

--- variations.js
+++ variations.js
@@ -1,4 +1,2 @@
 var v = document.location.search.split('&')[0]
-v.substring(v.length -1) === 'b'
-  ? localStorage.setItem('theme-ui-color-mode', 'dark')
-  : localStorage.setItem('theme-ui-color-mode', 'light')
+if(v.substring(v.length -1) === 'b') localStorage.setItem('theme-ui-color-mode', 'dark')
